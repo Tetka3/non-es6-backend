51351e3715c475e008ceeca354eb2c4dda645cd3
Titus Njema <njema3@gmail.com>
2024-08-15 21:02:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Product = require("./models/productModel")
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Yes yes again")
+})
+
+// GET all products
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({})
+    res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
+
+// Get a single product
+app.get("/products/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+})
+
+app.post("/products", async (req, res) => {   
+    // console.log(req.body)
+    // res.send(req.body) 
+    try {
+      const product = await Product.create(req.body);
+      res.status(200).json(product);    
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).json({message: error.message});
+    }
+  })
+
+mongoose.connect('mongodb+srv://njematitus:eQ7Vtj94osH2cKIF@cluster0.cwx5g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('Connected!')
+    app.listen(3000, () => console.log("Listening on port 3000"))
+  }).catch( (error) => {
+    console.log(error);
+  });
+
---
+++ models/productModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+},
+{
+    timestamps: true,
+});
+
+
+
+module.exports = mongoose.model("Product", productSchema);
